c11b458a9120567378db6fc0ec3352eed3c3eefc
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-07-14 11:48:07 +0900

    no message

---
+++ backrncs/routes/auth.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../schemas/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+router.post('/login',async (req,res,next)=>{
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email:email});
+        if(!user){ return res.status(401).json({error:"User already exists"}); }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){ return res.status(401).json({error:"password not match"}); }
+
+        const token = jwt.sign({
+            userId : user.id,
+            nickname : user.nickname,
+        },
+            " secret",
+            {expiresIn: "1h"}
+        );
+
+        res.json({token})
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+})
+
+module.exports = router;
