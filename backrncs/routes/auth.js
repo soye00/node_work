ba9589bbebd796797833c9538e1ddf36cdcddcaa
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-07-15 14:32:50 +0900

    no message

--- backrncs/routes/auth.js
+++ backrncs/routes/auth.js
@@ -4,6 +4,20 @@ const User = require('../schemas/users');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+
+router.post("/register", async (req, res, next) => {
+    try {
+        const {nickname, email, password} = req.body;
+        const hashed = await bcrypt.hash(password, 12);
+        const user = new User({email, password: hashed, nickname});
+        const result = await user.save();
+        return res.json(result);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+
 router.post('/login',async (req,res,next)=>{
     try {
         const {email, password} = req.body;
